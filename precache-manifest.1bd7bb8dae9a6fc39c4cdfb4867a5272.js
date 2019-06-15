116ab6381ae59d6c1387e6f9ef3d2b81f54ba3e4
mojila <mojila@github.com>
2019-06-16 01:56:56 +0700

    change web

---
+++ precache-manifest.1bd7bb8dae9a6fc39c4cdfb4867a5272.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658ba2c94e42b507784f90876d14d32a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fa875fe8f14be211546",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "50e226cdf2dcb7db05da",
+    "url": "/static/css/main.b648b12d.chunk.css"
+  },
+  {
+    "revision": "6fa875fe8f14be211546",
+    "url": "/static/js/2.6e1f52e9.chunk.js"
+  },
+  {
+    "revision": "50e226cdf2dcb7db05da",
+    "url": "/static/js/main.887fa5ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9845217913a94326abb8a78dd3aa3ef7",
+    "url": "/static/media/ellipse.98452179.svg"
+  },
+  {
+    "revision": "0fb397313e10d42cb15b909f4b7a8097",
+    "url": "/static/media/superhero.0fb39731.svg"
+  }
+]);
